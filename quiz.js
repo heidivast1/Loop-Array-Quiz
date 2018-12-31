c0c144fc78c05a0175465e10cc659b26aa21e21b
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2018-12-31 11:02:28 -0500

    broken quiz

--- quiz.js
+++ quiz.js
@@ -1,4 +1,25 @@
+var correctAnswers = 0;
+var question;
+var answer;
+var response;
+var html;
+var questions = [[ 'What period of art is Picasso most commonly associated with?', 'cubism' ],
+['What is the setting of Monets most famous painting?', 'garden'],
+['What is the oldest sculpture of a woman we have today?', 'venus of willendorf']
+];
 
 function print(message) {
   document.write(message);
 }
+
+for ( var i = 0; i < questions.length; i += 1) {
+    question = questions[i][0];
+    answer = questions[i][1];
+    response = answer.toLowercase(prompt(question));
+    if (response === answer) {
+      correctAnswers += 1;
+    }
+}
+
+html = "You got " + correctAnswers + " correct!";
+print(html);
