938ea990edadd98361b111b2d7bf587ff2d10d19
heidivast1 <43884561+heidivast1@users.noreply.github.com>
2019-01-04 10:11:19 -0500

    new guess display

--- quiz.js
+++ quiz.js
@@ -7,9 +7,23 @@ var questions = [[ 'What period of art is Picasso most commonly associated with?
 ['What is the setting of Monets most famous painting?', 'garden'],
 ['What is the oldest sculpture of a woman we have today?', 'venus of willendorf']
 ];
+var correct = [];
+var wrong = [];
+
+
+function buildList(arr) {
+  var listHTML = '<ol>';
+    for ( var i = 0; i < arr.length; i += 1 ) {
+      listHTML += '<li>' + arr[i] + '</li>';
+    }
+    listHTML += '</ol>';
+    return listHTML;
+}
+
 
 function print(message) {
-  document.write(message);
+  var outputDiv = document.getElementById('output');
+  outputDiv.innerHTML = message;
 }
 
 for ( var i = 0; i < questions.length; i += 1) {
@@ -18,8 +32,15 @@ for ( var i = 0; i < questions.length; i += 1) {
     response = prompt(question).toLowerCase();
     if (response === answer) {
       correctAnswers += 1;
+      correct.push(question);
+    } else {
+      wrong.push(question);
     }
 }
 
 html = "You got " + correctAnswers + " correct!";
+html += '<h2>You got these questions correct:</h2>';
+html += buildList(correct);
+html += '<h2>You got these questions wrong:</h2>';
+html += buildList(wrong);
 print(html);
